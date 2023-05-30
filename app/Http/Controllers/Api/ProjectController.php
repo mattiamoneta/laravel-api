f8bcabbe27bd77de6bd80f1ad0e38890124e5fd6
mattiamoneta <in.mattiamoneta@gmail.com>
2023-05-30 15:43:11 +0200

    Ok Milestone 2

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index(){
+        $projects = Project::with('technologies')->paginate(3);
+
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+
+    }
+}
