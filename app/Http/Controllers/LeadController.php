c6711914fc3b7f606c379867b82969aea254a52a
mattiamoneta <in.mattiamoneta@gmail.com>
2023-06-06 19:29:52 +0200

    Added Validation

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Lead;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\BoolContact;
@@ -10,9 +11,27 @@ use App\Mail\BoolContact;
 class LeadController extends Controller
 {
     public function store(Request $request){
-        
+
         $inputData = $request->all();
 
+        $validator = Validator::make($inputData,
+        [
+            'name' => 'required',
+            'email' => 'required',
+            'message' => 'required'
+        ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'errors' => $validator->errors()
+                ]
+            );
+        }
+        
+    
         $newLead = new Lead();
         $newLead->name = $inputData['name'];
         $newLead->email = $inputData['email'];
@@ -20,10 +39,12 @@ class LeadController extends Controller
         $newLead->save();
 
         $contactObject = new BoolContact($newLead);
+
         Mail::to('in.mattiamoneta@gmail.com')->send($contactObject);
     
         return response()->json([
-                'success' => true
+                'success' => true,
+                'errors' => $validator->errors()
             ]);
 
     }
